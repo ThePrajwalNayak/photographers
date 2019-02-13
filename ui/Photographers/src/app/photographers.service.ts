242b39b65778fd05e7e4b0e107fb18a36dfdaaba
Prajwal Nayak <theprajwalnayak@gmail.com>
2019-02-13 21:35:05 +0530

    Adding favicon

--- ui/Photographers/src/app/photographers.service.ts
+++ ui/Photographers/src/app/photographers.service.ts
@@ -7,16 +7,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class PhotographersService {
 
-BASE_URL = "http://pickphotographersapi.tk";
+BASE_URL = "http://pickphotographersapi.tk/api";
   // BASE_URL = 'http://localhost:3000';
   constructor(private http:HttpClient) { }
 
     getAllPhotographers() {
-        return this.http.get(this.BASE_URL + '/api/photographers');
+        return this.http.get(this.BASE_URL + '/photographers');
     }
 
      getAllPhotographersById(id) {
-        return this.http.get(this.BASE_URL + '/api/photographers/'+id);
+        return this.http.get(this.BASE_URL + '/photographers/'+id);
     }
 
 
