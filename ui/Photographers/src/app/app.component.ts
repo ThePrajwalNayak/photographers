adf841afa4417e9b608bc7b8c614098f046217f8
Prajwal Nayak <theprajwalnayak@gmail.com>
2019-03-21 15:35:14 +0530

    Removing unwwanted code

--- ui/Photographers/src/app/app.component.ts
+++ ui/Photographers/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  // template: string =`<img src="http://pa1.narvii.com/5722/2c617cd9674417d272084884b61e4bb7dd5f0b15_hq.gif" />`;
 }
