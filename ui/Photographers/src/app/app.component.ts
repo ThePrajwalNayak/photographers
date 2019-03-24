7e091a75b6b19f814e46bdf135be59be47345ce0
Prajwal Nayak <theprajwalnayak@gmail.com>
2019-03-24 11:46:10 +0530

    Adding back to top button

--- ui/Photographers/src/app/app.component.ts
+++ ui/Photographers/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   // template: string =`<img src="http://pa1.narvii.com/5722/2c617cd9674417d272084884b61e4bb7dd5f0b15_hq.gif" />`;
+
+  backToTop(){
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+  }
 }
