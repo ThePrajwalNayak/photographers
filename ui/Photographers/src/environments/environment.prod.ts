35e890abbdd3a480a8e6562f6e301015e589b784
Prajwal Nayak <theprajwalnayak@gmail.com>
2019-09-09 21:14:05 +0530

    Some changes

--- ui/Photographers/src/environments/environment.prod.ts
+++ ui/Photographers/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-   baseUrl : 'http://pickphotographersapi.tk/api'
+  //  baseUrl : 'http://pickphotographersapi.tk/api'
+  baseUrl : 'http://139.59.82.120:8080/api'
 };
--- ui/Photographers/src/environments/environment.ts
+++ ui/Photographers/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: true,
   // baseUrl : 'http://localhost:8080/api'
-  baseUrl : 'http://pickphotographersapi.tk/api'
+  // baseUrl : 'http://pickphotographersapi.tk/api'
+  baseUrl : 'http://139.59.82.120:8080/api'
 };
 
 /*
